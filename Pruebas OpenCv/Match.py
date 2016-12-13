164afaac9d5fe847dbcbb8c9cddf55c9da6fea05
pracknes <nestormp27@gmail.com>
2016-12-13 03:30:05 +0000

    asdf

--- Pruebas OpenCv/Match.py
+++ Pruebas OpenCv/Match.py
@@ -14,7 +14,7 @@ bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck = True)
 matches = bf.match(des1, des2)
 matches = sorted(matches, key = lambda x:x.distance)
 
-img3 = cv2.drawMatches(img1, kp1, img2, kp2, matches[:50], None, flags = 2)
+img3 = cv2.drawMatches(img1, kp1, img2, kp2, matches[:10], None, flags = 2)
 
 cv2.imshow("img", img1)
 cv2.imshow("img2", img2)
